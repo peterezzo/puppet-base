c47c9e55805acbc60873e547427fb440878005e6
Pete Ezzo <peter.ezzo@gmail.com>
2016-07-12 16:09:16 -0400

    add hostfile capabilities to base::dns

--- manifests/dns.pp
+++ manifests/dns.pp
@@ -1,15 +1,25 @@
-# do configuration of resolv.conf
+# do configuration of resolv.conf and /etc/hosts
+# nameservers - array of dns nameservers to configure
+# domains - array of domains to search
 class base::dns (
-  $configure_dns = "${module_name}::configure_dns",
   $nameservers = false,
-  $domain = false
+  $domain      = false,
 ) {
+  if $caller_module_name != $module_name {
+    fail("Use of private class ${name} by ${caller_module_name}")
+  }
 
-
-  if $configure_dns {
+  # configure DNS if this is a nix machine, by horribly bad detection
+  if ($::kernel != 'windows') {
     file { '/etc/resolv.conf':
-      ensure => present,
+      ensure  => present,
       content => template("${module_name}/resolv_conf.erb")
     }
+  } else {
+    fail("The ${name} module is not supported on an ${::osfamily} based system.")
   }
+
+  # setup host entries in addition to dns
+  $hosts = hiera_hash("${name}::hosts",{})
+  create_resources(service, $hosts)
 }
