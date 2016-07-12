8a60f1ec7d5ce7d722f656d6dcf93581f13a3690
Pete Ezzo <peter.ezzo@gmail.com>
2016-07-12 16:08:31 -0400

    add call protector and username check

--- manifests/admin_user.pp
+++ manifests/admin_user.pp
@@ -10,7 +10,14 @@ class base::admin_user (
   $ssh_key = false,
   $ssh_key_type = false
 ) {
-  if $username {
+  if $caller_module_name != $module_name {
+    fail("Use of private class ${name} by ${caller_module_name}")
+  }
+
+  if $username == false {
+    fail("${name} called without username set")
+  }
+
   # only support RHEL-clones and Ubuntu LTS at the moment
   case $::osfamily {
     'Debian': {
@@ -21,7 +28,7 @@ class base::admin_user (
       $groups = [ 'adm', 'wheel', 'systemd-journal' ]
     }
     default: {
-      fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
+      fail("The ${name} module is not supported on an ${::osfamily} based system.")
     }
   }
 
@@ -47,15 +54,15 @@ class base::admin_user (
     $sudoer = 'ALL'
   }
 
-  # add a sudoers file since wheel expects password normally
-  file { "/etc/sudoers.d/${username}-nopasswd":
+  # add a sudoers file just in case someone broke wheel
+  file { "/etc/sudoers.d/${username}":
     ensure  => 'present',
     content => "${username} ALL=(ALL) ${sudoer}\n",
     mode    => '0440',
     require => User[$username],
   }
 
-  # drop a key in if we have one defined
+  # drop a public key in if we have one defined
   if (ssh_key != false) and (ssh_key_type != false) {
     ssh_authorized_key { $username:
       user    => $username,
@@ -65,6 +72,7 @@ class base::admin_user (
     }
   }
 
+  # remove cloud or temporary setup users
   if $remove_default_users {
     $builtins = [ 'centos', 'ubuntu' ]
     user { $builtins:
@@ -72,5 +80,4 @@ class base::admin_user (
       managehome => true,
     }
   }
-}
 }
